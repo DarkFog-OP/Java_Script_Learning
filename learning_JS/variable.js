bb32278300b1dcd4ef1c0e0b7c655394964d1ede
DarkFog-OP <deepakshegavi.iitmandi@gmail.com>
2025-11-15 13:22:41 +0000

    updated

--- learning_JS/variable.js
+++ learning_JS/variable.js
@@ -9,6 +9,8 @@ const Account_ID="B23312";   //this to be kept unchanged if tried to change thro
 let Name = "Deepak"
 var Branch = "EP"
 Gender = "Male"   //Declaring wihtout a type = declaring with var type (Global) 
+let CGPA;        //If not value is assigned then default value is "UNDEFINED" which is different from NULL
+let SGPA=null    //null means the empty_ness that the developer choose to show that this thing is empty by choice
 
 
 console.table([Account_ID,Name,Branch,Gender]); //dislpays output in the tabular format easy to read and print
@@ -21,14 +23,14 @@ while "var" is a global variable changing it anywhere inside or outside of local
     let Name="RAJ";
     var Branch = "CSE"
     console.log("\n let & var variable changes in the local scope")
-    console.table([Account_ID,Name,Branch,Gender]);
+    console.table([Account_ID,Name,Branch,Gender,CGPA,SGPA]);
 
     let crush="KS";
     var happy=true;
 }
 
 //Note - let Name remains the same here as global while the branch got changed
-console.table([Account_ID,Name,Branch,Gender]);
+console.table([Account_ID,Name,Branch,Gender,CGPA,SGPA]);
 
 // console.log(crush) -> This throws error as the crush is a local var its not defined for this scope
 
