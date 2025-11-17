834a10209785735f079b3dd42de5ce1b4075a76d
DarkFog-OP <deepakshegavi.iitmandi@gmail.com>
2025-11-17 10:14:04 +0000

    studied about dates

---
+++ learning_JS/07_Date.js
@@ -0,0 +1,48 @@
+//This chapter is all about date and time which is a crucial part in the backend like state management and etc
+
+let today=new Date();
+console.log(today);   // odd looking outputs 2025-11-17T09:39:48.945Z
+
+//Note -> JS stores the date in millisecongds in the backend and the default reference date for the time calculation is 1970-01-01;
+
+//the dafault date
+console.log(new Date(0));
+
+//lets say x=12323413 milliseconds have passed till that day then
+console.log("After 12323413 days after default day => ",new Date(12323413).toLocaleString());
+
+
+today = new Date(); //fetches todasy date using the browser 
+
+//fetching different part of the time like day , date , housrs , seconds
+
+//use .getXXXX
+
+console.log(today.getDate()); // date 
+console.log(today.getDay());  //monday
+console.log(today.getMonth()); // month but returns 0 indexed number
+console.log(today.getHours()); //house
+
+
+//Assigning a date 
+
+//types of input to work = 2,3,4,6
+//2 => yyyy,mm
+//3 => yyyy,mm,dd
+//4 => yyyy,mm,dd,hh
+//5 => yyyy,mm,dd,hh,min
+//6 => yyyy,mm,dd,hh.min,ms
+
+let target = new Date(2028,10,12,3);
+
+console.log(new Date(2028,10,12,3).toLocaleString()); // 11/12/2028, 3:00:00 AM
+console.log(new Date(2028,10).toLocaleString()); // 11/1/2028, 12:00:00 AM
+console.log(new Date(2028,10,12,3,45,3).toLocaleString()) //11/12/2028, 3:45:03 AM
+
+
+//NOTE - date is of object typr
+// console.log(typeof target); => Object
+
+//JS stores the date in milliseconds(numbers) so to check if time is same or not just euate them 
+
+console.log(new Date().getTime()); //1763374386062 miliseconds have passed since the default date
